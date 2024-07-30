e20c5306a68f6368f1f77145332df96a50f6ac1a
Harikumar Shankar <harikum2494@gmail.com>
2024-07-30 15:09:52 -0400

    Initial commit - creating project with spring web dependency

---
+++ src/main/java/com/hari/restapi/Example1Application.java
@@ -0,0 +1,13 @@
+package com.hari.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Example1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Example1Application.class, args);
+	}
+
+}
