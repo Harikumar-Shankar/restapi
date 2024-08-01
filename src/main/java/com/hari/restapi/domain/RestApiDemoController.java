3cfee7b31bdea854b5e7680b370d2c466ab5e042
Harikumar Shankar <harikum2494@gmail.com>
2024-07-31 22:41:32 -0400

    Added endpoints for coffees with values on list

--- src/main/java/com/hari/restapi/domain/RestApiDemoController.java
+++ src/main/java/com/hari/restapi/domain/RestApiDemoController.java
@@ -1,11 +1,15 @@
 package com.hari.restapi.domain;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+// Elevated common URI path on all the endpoints to the class level
+@RequestMapping("/coffees")
 @RestController
 public class RestApiDemoController {
     private List<Coffee> coffees = new ArrayList<>();
@@ -16,9 +20,10 @@ public class RestApiDemoController {
                 new Coffee("Premium")));
     }
 
-    @GetMapping("/coffees/{id}")
+    // @GetMapping("/coffees/{id}")
+    @GetMapping("/{id}")
     public Optional<Coffee> getCoffeeById(@PathVariable final String id) {
-        for (var coffee: coffees) {
+        for (var coffee : coffees) {
             if (coffee.getId().equals(id)) {
                 return Optional.of(coffee);
             }
@@ -26,26 +31,38 @@ public class RestApiDemoController {
         return Optional.empty();
     }
 
-    @RequestMapping(value = "/coffees", method = RequestMethod.GET)
+    // @RequestMapping(value = "/coffees", method = RequestMethod.GET)
+    @GetMapping
     public Iterable<Coffee> getCoffees() {
         return coffees;
     }
 
-    @PostMapping(value="/coffees")
-    public Coffee addCoffee( @RequestBody final Coffee coffee) {
+    @PostMapping
+    // @PostMapping(value = "/coffees")
+    public Coffee addCoffee(@RequestBody final Coffee coffee) {
         coffees.add(coffee);
         return coffee;
     }
 
-    @PutMapping("/coffees/{id}")
-    public Coffee updateCoffee(@PathVariable String id, @RequestBody final Coffee coffee) {
+    // @PutMapping("/coffees/{id}")
+    @PutMapping("/{id}")
+    public ResponseEntity<Coffee> updateCoffee(@PathVariable String id, @RequestBody final Coffee coffee) {
         int coffeeIndex = -1;
-        for (Coffee c: coffees) {
-            if(c.getId().equals(id)) {
+        for (Coffee c : coffees) {
+            if (c.getId().equals(id)) {
                 coffeeIndex = coffees.indexOf(c);
                 coffees.set(coffeeIndex, coffee);
             }
         }
-        return coffeeIndex != -1 ? coffee : addCoffee(coffee);
+        // Based on IETF, GET method does not need status code, Post needs suggestion and PUT requires proper code
+        // We are returning 201(CREATED) if there is create and 200(OK) for updates to the existing.
+        // return coffeeIndex != -1 ? coffee : addCoffee(coffee);
+        return coffeeIndex == -1 ? new ResponseEntity<>(addCoffee(coffee), HttpStatus.CREATED) : new ResponseEntity<>(coffee, HttpStatus.OK);
+    }
+
+    // @DeleteMapping("/coffees/{id}")
+    @DeleteMapping("/{id}")
+    public void deleteCoffee(@PathVariable final String id) {
+        coffees.removeIf(coffee -> coffee.getId().equals(id));
     }
 }
